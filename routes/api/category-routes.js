84bc6acdf3df1b590dec94e17a61ffd832402205
Paul Chang <paulchang959@gmail.com>
2021-02-12 23:16:01 -0800

    final review

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -88,7 +88,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
-    const delCat = await Product.destroy({
+    const delCat = await Category.destroy({
       where: {
         id: req.params.id,
       },
